9c568901a488d7d35f4118afa7dbdbb291b209d9
Arktur7 <javare@ukr.net>
2021-10-10 20:43:55 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -23,7 +23,7 @@ if (temperature < -25|| temperature > 25) {
 console.log("Now you're ready to go outside!");
 
 const isCitizen = true;
-const age = 26;
+const age = 27;
 
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
